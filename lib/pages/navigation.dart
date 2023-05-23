5a6734b2a720e5967e0cd87aefad7741ca817519
jeongah <ji9390852@gmail.com>
2023-05-23 14:30:30 +0900

    Resolve merge conflict

--- lib/pages/navigation.dart
+++ lib/pages/navigation.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/Home_page.dart';
-import 'package:flutter_application_1/pages/Calendar_Page.dart';
-import 'package:flutter_application_1/pages/CounselPage.dart';
-import 'package:flutter_application_1/pages/MyPage.dart';
-import 'package:flutter_application_1/pages/Notification_page.dart';
+import './Home_page.dart';
+import './Calendar_Page.dart';
+import './CounselPage.dart';
+import './MyPage.dart';
+import './Notification_page.dart';
 
 class NavigationExample extends StatefulWidget {
   const NavigationExample({Key? key}) : super(key: key);
@@ -11,37 +11,41 @@ class NavigationExample extends StatefulWidget {
   @override
   State<NavigationExample> createState() => _NavigationExampleState();
 }
+
 class _NavigationExampleState extends State<NavigationExample> {
   int currentPageIndex = 0;
   List<String> notifications = []; //알림 리스트
-   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(
-          child: Text('HOOHA',
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
+          child: Text(
+            'HOOHA',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              //알림 아이콘 눌렀을 때 실행될 로직 추가(예정)
+              Navigator.push(
+                  // 새로운 페이지로 이동하는 메서드 (NotificationPage로 이동)
+                  context,
+                  MaterialPageRoute(
+                      //새로운 페이지를 생성
+                      //builder 함수를 통해 NotificationPage 인스턴스를 생성
+                      builder: (context) => NotificationPage(
+                          notifications:
+                              notifications))); //notifications 변수를 페이지로 전달
+            },
+          )
+        ], //actions
       ),
-    ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.notifications),
-          onPressed: (){
-            //알림 아이콘 눌렀을 때 실행될 로직 추가(예정)
-            Navigator.push( // 새로운 페이지로 이동하는 메서드 (NotificationPage로 이동)
-              context, 
-              MaterialPageRoute( //새로운 페이지를 생성
-              //builder 함수를 통해 NotificationPage 인스턴스를 생성
-                builder: (context) => NotificationPage(notifications: notifications) )
-                ); //notifications 변수를 페이지로 전달
-          },
-        )
-      ],  //actions
-      ),
-
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
