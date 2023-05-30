aa45a778c9f90083b3b8071772922d5d7cd5a1ad
jeongah <ji9390852@gmail.com>
2023-05-30 18:12:14 +0900

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './pages/navigation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 // ignore: non_constant_identifier_names
 String OPENAI_API_KEY = dotenv.env['OPEN_AI_API_KEY']!;
 // ignore: constant_identifier_names
 const String MODEL_ID = 'text-davinci-003';
 
+/// Cloud Firestore init
+FirebaseFirestore db = FirebaseFirestore.instance;
+
 void main() async {
   await dotenv.load(fileName: 'assets/config/.env');
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,54 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hooha/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // 앱 실행
+  runApp(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  // Firebase 앱 초기화
+  setUp(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
   });
+
+  // Firestore 인스턴스 가져오기
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // 나머지 코드 작성
+  final user = <String, dynamic>{
+    "first": "Ada",
+    "last": "Lovelace",
+    "born": 1815
+  };
+  print(user);
+
+  // Firestore에 문서 추가
+  db.collection("users").add(user).then((DocumentReference doc) =>
+      print('DocumentSnapshot added with ID: ${doc.id}'));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
 }
