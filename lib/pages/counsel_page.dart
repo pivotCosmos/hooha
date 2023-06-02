e71ee9c5e9a1c4a7569bb49d7970ddabb73333df
jeongah <ji9390852@gmail.com>
2023-06-02 11:10:56 +0900

    Display next chatbot message

--- lib/pages/counsel_page.dart
+++ lib/pages/counsel_page.dart
@@ -43,13 +43,16 @@ class _CounselPageState extends State<CounselPage> {
   /// 사용자에게 주어지는 선택지
   List<String> _options = [];
 
-  /// 선택지 버튼을 클릭하면 이어지는 다음 챗봇 메시지 번호
-  List<int> _nextMessagesNums = [];
+  /// 선택지 버튼을 클릭하면 이어질 챗봇 메시지 번호들
+  List<String> _nextMessagesNums = ['0'];
+
+  /// 다음에 띄워줄 챗봇 메시지 번호 (디폴트 '0')
+  //String _nextChatbotMsgNo = '0';
 
   // 디폴트 세팅들
-  final _defaultMessage = 'Welcome';
+  final _defaultMessage = '안녕하세요! 후하와 대화를 시작해 볼까요?';
   final _selectButtonMessage = 'Select a button';
-  final _defaultOptions = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+  final _defaultOptions = ['좋아요!'];
 
   /// 자동으로 스크롤이 내려가게 하기 위한 스크롤컨트롤러
   final ScrollController _scrollController = ScrollController();
@@ -141,9 +144,9 @@ class _CounselPageState extends State<CounselPage> {
     }
   }
 
-  /// Firestore에서 선택지 텍스트 가져오기
+  /// Firestore에서 선택지 텍스트, 다음 메시지 번호 가져오기
   /// return nextMsg, optionTxt
-  Future<Map<String, String>> _getOptionMsg(String msgNo) async {
+  Future<Map<String, String>> _getOptionMsgAndNextMsgNo(String msgNo) async {
     String collectionName = 'options'; // 컬렉션 이름
     String documentId = msgNo; // 문서 ID
 
@@ -204,18 +207,31 @@ class _CounselPageState extends State<CounselPage> {
   }
 
   /// 챗봇 메시지와 선택지 버튼 띄우기
-  /// option: 사용자가 클릭한 선택지 버튼 텍스트
-  void _showHoohaMsgAndUserOptions(String option) async {
+  /// option: 사용자가 클릭한 선택지 버튼 텍스트.
+  /// index: 사용자가 클릭한 버튼 인덱스
+  void _showHoohaMsgAndUserOptions(int buttonIndex) async {
+    print("Start of _showHoohaMsgAndUserOptions");
+
+    // 사용자가 클릭한 선택지 인덱스 확인하기
+    print("_nextMessagesNums=$_nextMessagesNums");
+
     // 사용자가 클릭한 선택지 텍스트를 버블로 띄워주기
+    String option = _options[buttonIndex];
     _addMessage(option);
+    print("clickedButtonIndex=$buttonIndex");
+
+    // 사용자가 클릭한 선택지에 연결된 메시지 번호 가져오기
+    String nextMsgNo = _nextMessagesNums[buttonIndex];
 
     // 시나리오 혹은 프롬프트로 분기 나누기
     // 분기 나눈 뒤에 API 호출 부분 주석 해제
 
+    // 챗봇 메시지 가져오기 (1.시나리오 2.프롬프트)
     // 1. 시나리오인 경우 DB에서 가져와서 _messages에 저장
-    Map<String, String> msgData = await _getChatbotMsg('0');
+    Map<String, String> msgData = await _getChatbotMsg(nextMsgNo);
     String? msgTxt = msgData['msgTxt'];
     _addMessage(msgTxt!);
+    print("using nextMsgNo(=$nextMsgNo), msgTxt=$msgTxt");
 
     // 2. 프롬프트인 경우 API 호출, AI 응답을 받아와서 _messages에 저장
     // _getAIResponse(option).then((aiResponse) {
@@ -224,28 +240,44 @@ class _CounselPageState extends State<CounselPage> {
     //   _addMessage('Error: ${error.toString()}');
     // });
 
-    // 옵션 값 가져와서 _options에 저장하기
+    // 챗봇 메시지에 딸린 선택지 옵션들 담기
     String? options = msgData['options'];
     List<String>? optionList = options?.split(' ');
+    print("options=$options");
+
+    // 선택지 텍스트, 다음 메시지 번호 담을 준비
     List<String> optTxtList = [];
+    List<String> nextMsgNoList = [];
+
+    // 선택지 반복문 돌리기
     for (var opt in optionList!) {
-      Map<String, String> optMap = await _getOptionMsg(opt);
+      // 선택지 텍스트, 다음 메시지 번호 가져오기
+      Map<String, String> optMap = await _getOptionMsgAndNextMsgNo(opt);
+      print("optMap=$optMap");
+
+      // 옵션 값 담기
       String? txt = optMap['optionTxt'];
       optTxtList.add(txt!);
+
+      // 선택지와 연결된 다음 메시지 번호 담기
+      String? nextMsgNo = optMap['nextMsg'];
+      nextMsgNoList.add(nextMsgNo!);
     }
 
     // 선택지 버튼 텍스트 업데이트
-    setButtonOptions(optTxtList);
+    setButtonOptions(optTxtList, nextMsgNoList);
+    print("End of _showHoohaMsgAndUserOptions");
   }
 
-  /// 사용자에게 제공할 선택지&다음 메시지 번호 세팅
-  void setButtonOptions(List<String>? options) {
+  /// 사용자에게 제공할 선택지와 선택지별 다음 메시지 번호 세팅
+  void setButtonOptions(List<String>? options, List<String>? nextMsgNums) {
     setState(() {
       /// 선택지 텍스트 세팅
       _options = options!;
 
       /// 선택지에 연결된 다음 메시지 번호 세팅
-      _nextMessagesNums = [0, 1, 2, 3];
+      _nextMessagesNums = nextMsgNums!;
+      print("_nextMessagesNums=$_nextMessagesNums");
     });
   }
 
@@ -290,7 +322,7 @@ class _CounselPageState extends State<CounselPage> {
             // 챗봇 응답 기다리는 동안 선택지 버튼 비활성화
             onPressed: isChatbotMessage
                 ? null
-                : () => _showHoohaMsgAndUserOptions(_options[i]),
+                : () => _showHoohaMsgAndUserOptions(i), // 버튼 인덱스 전달
             child: Text(_options[i]),
           ),
       ],
