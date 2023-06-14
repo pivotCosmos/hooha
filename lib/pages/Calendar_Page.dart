c40a373228c561b2ba5b16888960ce74be163a4f
Jindong Lee <dropnowon@gmail.com>
2023-06-14 14:01:16 +0900

    d

--- lib/pages/Calendar_Page.dart
+++ lib/pages/Calendar_Page.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
-import 'package:flutter_calendar_carousel/classes/event_list.dart';
+
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao;
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart'
     show CalendarCarousel;
